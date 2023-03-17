d936c6d87ecf1709f42741de160aa0acd5f81ca9
Timothy Ehlers <tehlers@expedia.com>
2023-03-16 19:15:16 -0500

    attempt to get 1.22 working

--- server/server.go
+++ server/server.go
@@ -613,8 +613,9 @@ func (s *Server) prepareServer(entryPointName string, entryPoint *configuration.
 				ReadTimeout:  readTimeout,
 				WriteTimeout: writeTimeout,
 				IdleTimeout:  idleTimeout,
-				ErrorLog:     httpServerLogger,
+				ErrorLog:     httpServerLogger
 			},
+			MaxConcurrentStreams: uint32(2000000),
 		},
 		listener,
 		nil
